2439c0a798ad594144f652fef0b7fdc6ccadd8db
aka2029 <asaks22@gmail.com>
2020-07-15 00:09:35 +0530

    new commit

--- Form-Validator/script.js
+++ Form-Validator/script.js
@@ -24,6 +24,48 @@ function isValidEmail(email) {
   return re.test(String(email).toLowerCase());
 }
 
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function(input) {
+    console.log(input);
+  });
+}
+
+// EventListener
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  checkRequired([username, email, password, password2]); //we passed an array cause it's better when keeping scalability and clean code in mind. Although we would have to go through the array to access elements.
+});
+
+/* This is basic form validator - but everything inside the event listener is not scalable.
+
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+// Show Success message
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+// Check email is valid - taken from StackOverFlow
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 // EventListener
 form.addEventListener("submit", function(e) {
   e.preventDefault();
@@ -54,3 +96,4 @@ form.addEventListener("submit", function(e) {
     showSuccess(password2);
   }
 });
+*/
