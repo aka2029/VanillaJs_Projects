442f72256aa4f52746b224bc253588e649acf12d
aka2029 <asaks22@gmail.com>
2020-07-21 15:10:48 +0530

    new commit

--- Movie-Seat-Booking/script.js
+++ Movie-Seat-Booking/script.js
@@ -6,9 +6,22 @@ const movieSelect = document.getElementById("movie");
 
 let ticketPrice = +movieSelect.value; // + is added just to make it a number otherwise it was a string. Check console.log(typeOf ticketPrice)
 
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex); //here stringify isn't req cause it's not an array or anything
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
 //Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  // For, localStorage... Copy selected seats into arr
+  // Map through array
+  // return a new array indexes
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat)); //map is similar to forEach it's just that it return an array
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex)); //since seatsIndex is an array hence we converted it into JSON.stringify
 
   const selectedSeatsCount = selectedSeats.length;
 
@@ -19,6 +32,7 @@ function updateSelectedCount() {
 // Movie select event
 movieSelect.addEventListener("change", e => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
