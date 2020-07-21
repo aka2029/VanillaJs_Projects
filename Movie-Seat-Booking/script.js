5d36347b8c0e24dcfdcd37f41262b5490cb2e501
aka2029 <asaks22@gmail.com>
2020-07-21 15:19:28 +0530

    new commit

--- Movie-Seat-Booking/script.js
+++ Movie-Seat-Booking/script.js
@@ -4,6 +4,8 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
+populateUI();
+
 let ticketPrice = +movieSelect.value; // + is added just to make it a number otherwise it was a string. Check console.log(typeOf ticketPrice)
 
 // Save selected movie index and price
@@ -29,6 +31,25 @@ function updateSelectedCount() {
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Get data from localStorage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 // Movie select event
 movieSelect.addEventListener("change", e => {
   ticketPrice = +e.target.value;
@@ -47,3 +68,6 @@ container.addEventListener("click", e => {
     updateSelectedCount();
   }
 });
+
+// Initial count and total set
+updateSelectedCount();
